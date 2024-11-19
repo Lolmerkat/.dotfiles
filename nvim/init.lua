bca36da3e69109f19084d0503583925e3848fc5a
Lolmerkat <97191104+Lolmerkat@users.noreply.github.com>
2024-11-19 14:38:12 +0100

    add harpoon

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,7 @@ require("config.lazy")
 require("config.remap")
 require("config.lsp")
 require("config.set")
+require("config.harpoon")
 
 vim.cmd([[colorscheme tokyonight]])
 
---
+++ nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,8 @@
+return {
+    'theprimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-telescope/telescope.nvim'
+    }
+}
