f3ba83fd80109790128916ed57327d40fd7f3a07
Lolmerkat <97191104+Lolmerkat@users.noreply.github.com>
2024-11-19 15:12:19 +0100

    remap changes

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -1,5 +1,20 @@
 vim.keymap.set("n", "<leader>pv", ':NvimTreeOpen<CR>', { desc = 'Open NvimTree', noremap = true, silent = true})
 
 local ts_builtin = require('telescope.builtin') 
-vim.keymap.set('n', "<leader>ff", ts_builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>L', ':Lazy<CR>', { desc = "Open Lazy.nvim", noremap = true, silent = true })
+vim.keymap.set('n', "<leader>ff", ts_builtin.find_files,
+    { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>L', ':Lazy<CR>',
+    { desc = "Open Lazy.nvim", noremap = true, silent = true })
+
+--tab keybinds
+vim.keymap.set('n', '<leader>Tn', ":tabnew<CR>",
+    { desc = "Open new Tab", noremap = true, silent = true})
+vim.keymap.set('n', '<leader>Tt', ":tabnew term<CR>", 
+    { desc = "Open new Terminal Tab", noremap = true, silent = true})
+vim.keymap.set('n', '<leader>Td', ":tabclose<CR>",
+    { desc = "Close active Tab", noremap = true, silent = true})
+
+vim.keymap.set('n', '<leader>T<', ":tabprevious<CR>",
+    { desc = "Switch to prev. Tab", noremap = true, silent = true})
+vim.keymap.set('n', '<leader>T>', ":tabnext<CR>",
+    { desc = "Switch to next Tab", noremap = true, silent = true})
