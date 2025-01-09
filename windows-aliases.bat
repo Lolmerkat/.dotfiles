fb5d328b86d900174ee212aabacf50e184cd2d4b
Lolmerkat <git@lolmerkat.cc>
2025-01-09 19:08:29 +0100

    add Windows Aliases

---
+++ windows-aliases.bat
@@ -0,0 +1,17 @@
+:: File Aliases
+
+DOSKEY alias_file=%config_home%\windows-aliases.bat
+
+:: Directory Aliases
+
+DOSKEY config=cd %config_home%
+DOSKEY edit-config=nvim %config_home%
+
+:: Command aliases
+
+DOSKEY ls=dir /B $*
+DOSKEY mv=move $*
+DOSKEY aliases=nvim alias_file
+DOSKEY alias=echo DOSKEY $1=$2 >> aliad_file
+
+:: Command-added aliases
