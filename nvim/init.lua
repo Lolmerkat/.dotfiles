a163981990068d8079bfd239965475df0aed9ce3
Lolmerkat <git@lolmerkat.cc>
2025-01-23 23:58:08 +0100

    Add: Autocommands
    (Also, autocommands actually are loaded in init.lua, lol)

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,5 +4,6 @@ require("config.lsp")
 require("config.set")
 require("config.harpoon")
 require("config.lualine")
+require("config.autocommand")
 
 vim.cmd([[colorscheme tokyonight]])
--- nvim/lua/config/autocommand.lua
+++ nvim/lua/config/autocommand.lua
@@ -15,3 +15,22 @@ vim.api.nvim_create_autocmd("FileType", {
         end)
     end
 })
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+    pattern = { "markdown", "md" },
+    callback = function ()
+        vim.keymap.set(
+            { 'i', 'n' }, "<Space><Space>m", function ()
+                vim.cmd('Markview splitToggle')
+            end, { buffer = true, silent = true }
+        )
+        vim.cmd('Markview splitEnable')
+    end
+})
+
+vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
+    pattern = { "markdown", "md" },
+    callback = function ()
+        vim.cmd('Markview splitDisable')
+    end
+})
