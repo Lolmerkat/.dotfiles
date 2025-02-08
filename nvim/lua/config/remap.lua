ec089a98279135c4550bd5fe2324a71cd2704b0a
Lolmerkat <git@lolmerkat.cc>
2025-02-08 14:52:53 +0100

    Change: Remap
    Add remap for: <C-h>
    -> toggle hlsearch
    
    Remove harpoon remaps except <C-a>
    
    Restructure remap.lua

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -1,5 +1,32 @@
+-- Vim general
+vim.keymap.set({'i', 'n'}, '<C-S>w', '<cmd>write<CR>'
+    ,{ desc = "Write", noremap = true, silent = false })
+vim.keymap.set({'i', 'n'}, '<C-S>s',
+    function ()
+        vim.cmd('write')
+        vim.cmd('source %')
+    end
+    ,{ desc = "Write & Source", noremap = true, silent = false })
+vim.keymap.set('n', '<C-h>', '<cmd>set hlsearch!<CR>',
+    { desc = "Toggle search highlighting", noremap = true, silent = true })
+vim.keymap.set({ 'i', 'v' }, "hj", '<ESC>',
+    { desc = 'Escape', noremap = false, silent = true })
+
+-- Vim tabs
+vim.keymap.set('n', '<leader>Tn', ":tabnew<CR>",
+    { desc = "Open new Tab", noremap = true, silent = true})
+vim.keymap.set('n', '<leader>Tt', ":tab term<CR>",
+    { desc = "Open new Terminal Tab", noremap = true, silent = true})
+vim.keymap.set('n', '<leader>Td', ":tabclose<CR>",
+    { desc = "Close active Tab", noremap = true, silent = true})
+
+vim.keymap.set('n', '<leader>T<', ":tabprevious<CR>",
+    { desc = "Switch to prev. Tab", noremap = true, silent = true})
+vim.keymap.set('n', '<leader>T>', ":tabnext<CR>",
+    { desc = "Switch to next Tab", noremap = true, silent = true})
+
+-- NvimTree
 vim.keymap.set("n", "<leader>pv", ':NvimTreeToggle<CR>', { desc = 'Toggle NvimTree', noremap = true, silent = true})
-vim.keymap.set({ 'i', 'v' }, "hj", '<ESC>', { desc = 'Escape', noremap = false, silent = true })
 
 -- Telescope
 local ts_builtin = require('telescope.builtin')
@@ -22,37 +49,6 @@ vim.keymap.set('n', '<leader>M', '<cmd>Mason<CR>',
 local harpoon = require("harpoon")
 vim.keymap.set('n', "<C-a>", function() harpoon:list():add() end,
     {desc = "Add to harpoon"})
-
-vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
-
--- Vim general
-vim.keymap.set({'i', 'n'}, '<C-S>w',
-    function ()
-        vim.cmd('write')
-    end
-    ,{ desc = "Write", noremap = true, silent = false })
-vim.keymap.set({'i', 'n'}, '<C-S>s',
-    function ()
-        vim.cmd('write')
-        vim.cmd('source %')
-    end
-    ,{ desc = "Write & Source", noremap = true, silent = false })
-
--- Vim tabs
-vim.keymap.set('n', '<leader>Tn', ":tabnew<CR>",
-    { desc = "Open new Tab", noremap = true, silent = true})
-vim.keymap.set('n', '<leader>Tt', ":tab term<CR>",
-    { desc = "Open new Terminal Tab", noremap = true, silent = true})
-vim.keymap.set('n', '<leader>Td', ":tabclose<CR>",
-    { desc = "Close active Tab", noremap = true, silent = true})
-
-vim.keymap.set('n', '<leader>T<', ":tabprevious<CR>",
-    { desc = "Switch to prev. Tab", noremap = true, silent = true})
-vim.keymap.set('n', '<leader>T>', ":tabnext<CR>",
-    { desc = "Switch to next Tab", noremap = true, silent = true})
 
 -- Obsidian
 vim.keymap.set('n', 'gf', function()
