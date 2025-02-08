0905087200a26924fe4291166a62766853fa66d8
Lolmerkat <git@lolmerkat.cc>
2025-02-08 14:26:04 +0100

    Add: Theme on lualine

--- nvim/lua/config/lualine.lua
+++ nvim/lua/config/lualine.lua
@@ -3,10 +3,10 @@ local attached_lsps = {}
 local function refresh_attached_lsps()
   -- Get the current buffer number
   local bufnr = vim.api.nvim_get_current_buf()
-  
+
   -- Fetch the attached clients for the current buffer
   local clients = vim.lsp.get_active_clients({ bufnr = bufnr })
-  
+
   -- Extract client names
   attached_lsps[bufnr] = {}
   for _, client in pairs(clients) do
@@ -23,19 +23,19 @@ end
 local function setup_lsp_refresh_autocommands()
   -- Define an autocommand group
   local group_id = vim.api.nvim_create_augroup("LSPAttachRefresh", { clear = true })
-  
+
   -- Run on buffer enter to list attached LSPs
   vim.api.nvim_create_autocmd("BufEnter", {
     group = group_id,
     callback = refresh_attached_lsps,
   })
-  
+
   -- Run when an LSP client attaches
   vim.api.nvim_create_autocmd("LspAttach", {
     group = group_id,
     callback = refresh_attached_lsps,
   })
-  
+
   -- Run when an LSP client detaches
   vim.api.nvim_create_autocmd("LspDetach", {
     group = group_id,
@@ -55,5 +55,6 @@ require("lualine").setup({
                 return "LSP: " .. table.concat(lsps, ", ")
             end
         }
-    }
+    },
+    theme = 'tokyonight'
 })
