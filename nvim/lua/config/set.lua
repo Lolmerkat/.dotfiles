73a1a13c26aa3a3ebe9fcf5bf117908075c47ad6
Lolmerkat <git@lolmerkat.cc>
2025-02-10 00:49:40 +0100

    Add: Folding

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -19,3 +19,12 @@ vim.opt.incsearch = true
 
 vim.opt.spelllang = { 'en', 'de' }
 
+-- Folding
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldcolumn = "0"
+vim.opt.foldtext = ""
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = -1
+vim.opt.foldnestmax = 4
+
