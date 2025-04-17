72af6b28d43a587960203f9953523b8070da775e
Lolmerkat <git@lolmerkat.cc>
2025-04-17 04:16:23 +0200

    Fix: ccc pattern matching

--- nvim/lua/config/autocommand/ccc.lua
+++ nvim/lua/config/autocommand/ccc.lua
@@ -1,13 +1,13 @@
 -- SCSS / SASS
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-    pattern = { "*.scss", "*.sass", "*.css", "*.svelte", "*.html" },
+    pattern = { "scss", "sass", "css", "svelte", "html" },
     callback = function()
         vim.cmd('CccHighlighterEnable')
     end
 });
 
 vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
-    pattern = { "*.scss", "*.sass", "*.css", "*.svelte", "*.html" },
+    pattern = { "scss", "sass", "css", "svelte", "html" },
     callback = function()
         vim.cmd('CccHighlighterDisable')
     end
