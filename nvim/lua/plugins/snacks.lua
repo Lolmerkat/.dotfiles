0f116a3baa5fa460dbc580a08d8c7a376904f9a6
Lolmerkat <git@lolmerkat.cc>
2025-04-18 02:18:21 +0200

    Change: Enable Zen mode

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -18,10 +18,20 @@ return {
                 }
             }
         },
+        quickfile = {
+            enabled = true
+        },
+        zen = {
+            enabled = true,
+            show = {
+                statusline = true
+            }
+        }
     },
     keys = {
         { "<leader>pv", function() Snacks.explorer() end, desc = "File Explorer" },
         { "<leader>ff", function() Snacks.picker.files() end, desc = "File Finder" },
-        { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" }
+        { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
+        { "<leader>z", function() Snacks.zen() end, desc = "Toggle Zen Mode"},
     }
 }
