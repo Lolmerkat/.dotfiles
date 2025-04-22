aa5ad82aefa78db6a1d9a5f316c8878775ef8f6d
Lolmerkat <git@lolmerkat.cc>
2025-04-22 16:48:45 +0200

    Fix: cfg windows alias

--- utils/windows-aliases.bat
+++ utils/windows-aliases.bat
@@ -7,7 +7,7 @@ SET alias_file=%XDG_CONFIG_HOME%\utils\windows-aliases.bat
 
 :: Directory Aliases
 
-DOSKEY cfg=nvim %XDG_CONFIG_HOME%
+DOSKEY cfg=nvim --cmd "cd %XDG_CONFIG_HOME%"
 DOSKEY aliases=nvim %alias_file%
 DOSKEY dskt=cd "%USERHOME%/Desktop"
 
