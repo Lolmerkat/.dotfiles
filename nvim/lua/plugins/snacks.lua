cdcdc11410409faeee7f4f064a1d8ac8f192c2bb
Lolmerkat <git@lolmerkat.cc>
2025-05-06 15:45:20 +0200

    Add: Snacks image and lines picker

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -7,6 +7,9 @@ return {
         explorer = {
             replace_netrw = true
         },
+        image = {
+            enabled = true
+        },
         picker = {
             enabled = true,
             main = {
@@ -15,8 +18,13 @@ return {
             sources = {
                 explorer = {
                     hidden = true
+                },
+                lines = {
+                    layout = {
+                        preset = "default"
+                    }
                 }
-            }
+            },
         },
         quickfile = {
             enabled = true
@@ -30,8 +38,10 @@ return {
     },
     keys = {
         { "<leader>pv", function() Snacks.explorer() end, desc = "File Explorer" },
+        { "<leader>pp", function() Snacks.picker() end, desc = "Picker List" },
         { "<leader>ff", function() Snacks.picker.files() end, desc = "File Finder" },
         { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
+        { "<leader>fl", function() Snacks.picker.lines() end, desc = "Find Lines" },
         { "<leader>ft", function() Snacks.picker.todo_comments() end, desc = "Find Todo Comments" },
         { "<leader>z", function() Snacks.zen() end, desc = "Toggle Zen Mode"},
     }
