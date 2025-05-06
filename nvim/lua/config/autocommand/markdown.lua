80994f2a9ad71738990ed41e7b8c55b39928d71e
Lolmerkat <git@lolmerkat.cc>
2025-05-06 15:45:30 +0200

    Add: Documentation comments

--- nvim/lua/config/autocommand/markdown.lua
+++ nvim/lua/config/autocommand/markdown.lua
@@ -2,18 +2,20 @@
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
     pattern = { "*.markdown", "*.md" },
     callback = function ()
+        -- toggle markview split
         vim.keymap.set(
             { 'n' }, "<leader>mm", function ()
                 vim.cmd('Markview splitToggle')
             end, { buffer = true, silent = true }
         )
 
+        -- compile pandoc
         vim.keymap.set('n', "<leader>cp", function ()
                 local current_file = vim.fn.expand('%')
                 local output_file = vim.fn.expand('%:r') .. '.pdf'
                 local cmd = string.format('pandoc %s -s -o %s', current_file, output_file)
                 vim.fn.system(cmd)
-               
+
                 local time = os.date("*t")
                 local hour = time.hour
                 local minute = time.min
--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -11,12 +11,13 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
 vim.opt.termguicolors = true
 
+-- max line length
 vim.opt.colorcolumn = "80"
+
 vim.opt.incsearch = true
 
 vim.opt.spelllang = { 'en', 'de' }
