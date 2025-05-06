1ee375dca4300b2b008e0b518f7742e3477fc2dc
Lolmerkat <git@lolmerkat.cc>
2025-05-06 15:42:54 +0200

    Add: Remap to open vim.diagnostic float

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -11,6 +11,8 @@ vim.keymap.set('n', '<C-h>', '<cmd>set hlsearch!<CR>',
     { desc = "Toggle search highlighting", noremap = true, silent = true })
 vim.keymap.set({ 'i', 'v' }, "hj", '<ESC>',
     { desc = 'Escape', noremap = false, silent = true })
+vim.keymap.set('n', 'J', function() vim.diagnostic.open_float() end
+    ,{ desc = "Dignostic: Open Float", noremap = false, silent = true })
 
 -- Vim tabs
 vim.keymap.set('n', '<leader>Tn', ":tabnew<CR>",
@@ -46,16 +48,6 @@ end, { noremap = false, expr = true })
 -- Ccc
 vim.keymap.set({'n'}, '<leader>pc', "<cmd>CccPick<CR>",
     { desc = "Open Ccc picker", noremap = true, silent = true })
-
--- LuaSnip
-vim.keymap.set({ "i", "s" }, "<Tab>", function ()
-    local ls = require("luasnip")
-    if ls.expand_or_jumpable() then
-        ls.expand_or_jump()
-    else
-        return "<Tab>"
-    end
-end, { silent = true, expr = true })
 
 -- LSP
 vim.keymap.set('n', "<leader>lR", "<cmd>LspRestart<CR>",
