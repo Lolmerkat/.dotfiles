6fd3e0621cbeede0b3f24e9196209edfbf26022f
Lolmerkat <git@lolmerkat.cc>
2025-06-06 03:03:42 +0200

    Fix LSP

--- nvim/lua/config/lsp/core.lua
+++ nvim/lua/config/lsp/core.lua
@@ -2,13 +2,16 @@ local lsp = require("lspconfig")
 local blink = require("blink.cmp")
 
 return {
-    setup = function()
-        require("mason-lspconfig").setup_handlers({
-            function(server)
-                lsp[server].setup({
-                    capabilities = blink.get_lsp_capabilities()
-                })
-            end
-        })
-    end
+	setup = function()
+		local lsp = require("lspconfig")
+		require("mason-lspconfig").setup({
+			handlers = {
+				function (server_name)
+					lsp[server_name].setup({
+						capabilities = blink.get_lsp_capabilities()
+					})
+				end,
+			}
+		})
+	end
 }
